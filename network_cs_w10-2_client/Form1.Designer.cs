c9a2ab53c5738561493497a7f96741d027b77ca9
walter <walter13467@outlook.com>
2022-04-25 11:26:54 +0800

    2

--- network_cs_w10-2_client/Form1.Designer.cs
+++ network_cs_w10-2_client/Form1.Designer.cs
@@ -77,6 +77,7 @@ namespace network_cs_w10_2_client
             this.button1.TabIndex = 3;
             this.button1.Text = "登入伺服器";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
--- network_cs_w10-2_client/Form1.cs
+++ network_cs_w10-2_client/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace network_cs_w10_2_client
 {
@@ -15,6 +17,37 @@ namespace network_cs_w10_2_client
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Socket T; //通訊物件
+        string User; //使用者
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string IP = textBox1.Text;
+            int Port = int.Parse(textBox2.Text);
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(IP), Port);
+
+            T = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            User = textBox3.Text;
+
+            try
+            {
+                T.Connect(EP);
+                Send("0" + User);
+            }
+            catch
+            {
+                MessageBox.Show("無法連上伺服器");
+                return;
+            }
+            button1.Enabled = false;
+        }
+        private void Send(string Str)
+        {
+            byte[] B = Encoding.Default.GetBytes(Str);
+            T.Send(B, 0, B.Length, SocketFlags.None);
         }
     }
 }
